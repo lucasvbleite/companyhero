1733104b5c6e708ab1226df6819fb4612fc78536
Lucas Leite <lucasvbleite@gmail.com>
2021-05-07 07:57:02 -0300

    requirements

--- teste/settings.py
+++ teste/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
     'rest_framework',
     'django_filters',
     'api',
-    'gunicorn',
 ]
 
 MIDDLEWARE = [
